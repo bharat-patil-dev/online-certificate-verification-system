b5d27be475adce15f46ba2092b9f58661aa4b939
Bharat Patil <shub773192@gmail.com>
2026-09-08 14:05:57 +0530

    Allow Vercel frontend in CORS

--- src/main/java/com/bharat/online_certificate_verification_system/config/SecurityConfig.java
+++ src/main/java/com/bharat/online_certificate_verification_system/config/SecurityConfig.java
@@ -95,8 +95,10 @@ public class SecurityConfig {
                 new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
-        );
+                List.of(
+                        "http://localhost:5173",
+                        "https://online-certificate-verification-fro.vercel.app")
+                );
 
         configuration.setAllowedMethods(
                 List.of(
